c82cd664363609b12595b8ffc7ae0f2914aaeb3d
mvl3 <mvl3@mvl3s-Mac-mini.local>
2019-09-23 16:22:12 +0900

    first commit

---
+++ src/HistorySample.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+const HistorySample = (props) => {
+
+    const handleGoBack = () => {
+        props.history.goBack();
+    };
+
+    const handleGoHome = () => {
+        props.history.push('/');
+    };
+
+    return (
+        <div>
+            <button onClick={handleGoBack}>back</button>
+            <button onClick={handleGoHome}>home</button>
+        </div>
+    )
+};
+
+export default HistorySample;
---
+++ src/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const data = {
+    geonu: {
+        name: '건우',
+        text: '건우입니다'
+    },
+    geonho: {
+        name: '건호',
+        text: '건호입니다'
+    },
+};
+
+const Profile = ({match}) => {
+    const {username} = match.params;
+    const profile = data[username];
+
+    if(!profile){
+        return <div>존재하지 않습니다.</div>
+    }
+
+    return(
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.text}</p>
+        </div>
+    )
+};
+
+export default Profile;
